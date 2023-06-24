6c9644873355f33bd56808ba170933a884dad9cc
Muthuinc <smramalingamm@gmail.com>
2023-06-24 14:34:36 +0530

    read readme

--- Replace word/replace.sh
+++ Replace word/replace.sh
@@ -3,21 +3,24 @@ set -e
 filename=sample.txt
 echo "From which line you want to find and replace a word. Enter the line number :"
 read b 
+echo "Enter the word to look in the line for script execution "
+read e
 echo "Enter the word you want to replace : "
 read c
 echo "Enter the word new word :"
 read d
+
 n=1
 while read line;
 do
   if  [ $n -ge $b ];
   then
-     if ( grep -q -w "${c}" <<< "$line" ) then
-      regex="^(.*)\b$c\b(.*)$"
+     if ( grep -q -w "${e}" <<< "$line" ) then
+      regex="^(.*)\b$e\b(.*)$"
       [[ "$line" =~ $regex ]]
        if [[ $? == 0 ]]
         then
-          echo "successful"
+         # echo "successful"
           # sed -i "s|${c}|$d|" sample.txt
           sed  -i "${n}s/${c}/$d/" $filename >> /dev/null 2>&1 
         #   cat sample.txt
