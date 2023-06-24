36fdc2ff4d78f1d2f4fd3746393b1468d3b773a2
Muthuinc <smramalingamm@gmail.com>
2023-06-24 14:39:54 +0530

    read readme

--- Replace word/replace.sh
+++ Replace word/replace.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
+
+
 set -e
+
+
 filename=sample.txt
+
+
 echo "From which line you want to find and replace a word. Enter the line number :"
 read b 
 echo "Enter the word to look in the line for script execution "
